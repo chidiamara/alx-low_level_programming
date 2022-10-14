e131a8410268ad003eebf00d8891561301f6466d
Chidi <chidiamaraekejiuba@gmail.com>
2022-10-13 20:42:09 -0700

    using puts func

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**Using the builtin function 'puts' to print line to standard output*/
+int main(void) 
+{
+puts("\"Programming is like building a multilingual puzzle"
+);
+return 0;
+}
