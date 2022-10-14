ceb2566b7d616ae9a776f4253947d2e66bdb51a8
Chidi <chidiamaraekejiuba@gmail.com>
2022-10-13 20:47:16 -0700

    i!/bin/bash using put to print
    The function prints the message to standard output using put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**Using the builtin function 'puts' to print line to standard output*/
-int main(void) 
+int main(void) // contains a sentence
 {
 puts("\"Programming is like building a multilingual puzzle"
 );
-return 0;
+return (0);
 }
