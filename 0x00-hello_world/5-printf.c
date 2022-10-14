e6557a7462135dcdfb6a84335177041927be3114
Chidi <chidiamaraekejiuba@gmail.com>
2022-10-13 21:54:37 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - use printf to print a string
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	printf(
+"with proper grammar, but the outcome i a piece of art,\n"
+);
+return (0);
+}
