f0e22f8c2f98eb7a094b6eec9f0178dae2c8ff06
Chidi <chidiamaraekejiuba@gmail.com>
2022-10-14 22:32:16 -0700

    base 10
    print songle digit numbers of base 10 starting from 0

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+/**
+ * main - print all single digit numbers of base 10 starting from 0,
+ * followed by a new line, using putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
