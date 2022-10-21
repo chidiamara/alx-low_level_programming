05d4bb8e273b545e149919c3a3ff733df52178aa
Chidi <chidiamaraekejiuba@gmail.com>
2022-10-21 06:32:55 -0700

    print fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include "main.h"
+/*#include "main.h"*/
 #include <stdio.h>
 
 /**
@@ -12,7 +12,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
 			printf(" FizzBuzz");
