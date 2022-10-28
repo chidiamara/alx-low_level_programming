f355ea7766295c3401d643868d7363a8d386254b
Chidi <chidiamaraekejiuba@gmail.com>
2022-10-27 19:58:39 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: destination for resulting string
+ * @src: source string to dest
+ *
+ * Return: destination string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i]; i++)
+	for (j = 0; src[j]; j++)
+		dest[i + j] = src[j];
+	dest[i + j] = '\0';
+
+	return (dest);
+}
