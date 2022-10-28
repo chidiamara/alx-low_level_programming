0f339710ee9660db82a8f5abc5df860f18ceab5c
Chidi <chidiamaraekejiuba@gmail.com>
2022-10-27 21:01:53 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings and return number of differences
+ * @s1: string 1 to check
+ * @s2: string 2 to check
+ *
+ * Return: number of differences
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] && s2[i]; i++)
+	{
+		if (s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+		else if (s1[i] > s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
