98affb28806955a0a05400057a22a93c727501a2
Chidi <chidiamaraekejiuba@gmail.com>
2022-10-27 21:58:56 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,6 +10,22 @@
 
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = src[j];
+
 	return (dest);
 }
