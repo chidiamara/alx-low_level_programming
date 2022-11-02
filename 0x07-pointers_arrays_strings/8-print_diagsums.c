41150815645cf8dae413ef82b1f0b644fedf9380
Chidi <chidiamaraekejiuba@gmail.com>
2022-11-01 21:55:58 -0700

    task 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*print_diagsums - prints sum of diags
+*@a: pointer to 2d array
+*@size: size of the array
+*/
+
+void print_diagsums(int *a, int size)
+{
+int i, sum 1, sum2;
+
+sum1 = 0;
+sum2 = 0;
+
+for (i = 0; i < size; i++)
+{
+sum1 = sum1 + *(a + (i + i * size));
+sum2 = sum2 + *(a + ((i + 1) * (size - 1)));
+}
+printf("%d, %d\n", sum1, sum2);
+}
