6f6c0121931b1e80c20eba647582e7b4108aca03
Chidi <chidiamaraekejiuba@gmail.com>
2022-11-04 22:07:28 -0700

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - prints all arguments, one argument per line
+*@argc: int
+*@argv: an array of strings
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
