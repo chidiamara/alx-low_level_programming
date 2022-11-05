fc07aeb22a003652355207e7ea9fba719b8dcc0c
Chidi <chidiamaraekejiuba@gmail.com>
2022-11-04 21:38:01 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+* main - print the name of the program followed by a new line
+*@argc: int
+*@argv: an array of strings
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,2 @@
+#ifndef MAIN_H                                 #define MAIN_H                                                                                int string_isdigit(char *s);
+                                               #endif
