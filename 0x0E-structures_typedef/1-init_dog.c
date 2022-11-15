550acdf34755bcb69ca9259217266a9b61ca91c2
Chidi <chidiamaraekejiuba@gmail.com>
2022-11-14 19:42:13 -0800

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,13 +8,13 @@
 *@age: float
 *@owner: pointer to a string
 */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d == '\0')
-{
-return;
-}
-d->name = name;
-d->age = age;
-d->owner = owner;
+	if (d == NULL)
+		d = (malloc(sizeof(struct dog)));
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
