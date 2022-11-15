09288c049e37c5923860f0fb069aea7869ec165e
Chidi <chidiamaraekejiuba@gmail.com>
2022-11-14 19:32:58 -0800

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - Each dog has the following
+*@name: name of dog
+*@age: age of dog
+*@owner: owner name
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+#endif
