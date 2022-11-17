fba1beb88fb6c7cf97f48ac93669e2711137c4be
Chidi <chidiamaraekejiuba@gmail.com>
2022-11-17 04:58:35 -0800

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print the string "name"
+ * @name: string to pass through f function
+ * @f: function that prints strings
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name)
+		return;
+	if (f)
+		f(name);
+}
