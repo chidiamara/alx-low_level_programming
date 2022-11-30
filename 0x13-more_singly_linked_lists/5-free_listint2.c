a04643bfc3ffa6ece41da4ba6e92b3f47b0bf70b
Chidi <chidiamaraekejiuba@gmail.com>
2022-11-29 19:15:17 -0800

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+* free_listint2 - frees a linked list
+* @head: head of the list
+*/
+void free_listint2(listint_t **head)
+{
+listint_t *storage;
+if (head == NULL)
+return;
+while (*head)
+{
+storage = (*head)->next;
+free(*head);
+*head = storage;
+}
+}
