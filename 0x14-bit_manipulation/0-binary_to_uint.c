c7300955280da8da5f4b9210ada280077c346054
Chidi <chidiamaraekejiuba@gmail.com>
2022-12-01 19:10:20 -0800

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* binary_to_uint - convert binary number into an integer
+* @b: string passing into function
+*
+* Return: decimal number if success, 0 if non binary num found
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec;
+
+	if (!b)
+		return (0);
+
+	for (dec = 0; *b; b++)
+	{
+		if (*b == '1')
+			dec = (dec << 1) | 1;
+		else if (*b == '0')
+			dec = dec << 1;
+		else
+			return (0);
+	}
+	return (dec);
+}
