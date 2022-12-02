f40ef97197469c4103b13f31760c750a113228ac
Chidi <chidiamaraekejiuba@gmail.com>
2022-12-01 19:32:48 -0800

    task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* flip_bits - calculate difference between two numbers
+* @n: number 1
+* @m: number 2 to "flip" bits from num 1
+*
+* Return: bits calculated
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int diff, bits = 0;
+
+	for (diff = n ^ m; diff; diff = diff >> 1)
+		bits += diff & 1;
+
+	return (bits);
+}
