bbb6b43af363d93e7b275db42789d258344e400a
Chidi <chidiamaraekejiuba@gmail.com>
2022-12-01 19:16:06 -0800

    task 1
    Convert decimal to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* print_binary - convert decimal to binary
+* @n: number to convert
+*
+* Return: void
+*/
+
+void print_binary(unsigned long int n)
+{
+	int i;
+	unsigned long int place;
+
+	if (n < 2)
+	{
+		_putchar(n + '0');
+		return;
+	}
+
+	for (i = 63; i >= 0; i--)
+	{
+		place = n >> i;
+
+		if (place)
+		{
+			if (place & 1)
+				_putchar('1');
+			else
+				_putchar('0');
+		}
+	}
+}
