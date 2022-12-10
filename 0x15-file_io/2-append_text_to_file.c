21815a7c7db53c6740d0784a4ae869680a3a233b
Chidi <chidiamaraekejiuba@gmail.com>
2022-12-09 20:51:54 -0800

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,28 +8,29 @@
 * Return: 1 on success, -1 on fail
 */
 
-int append_text_to_file(const char *filename, char *text_content)
-{
-	int i, wcount, fd;
 
-	if (!filename)
-		return (-1);
+int append_text_to_file(const char *filename, char *text_content) {
+  if (!filename) {
+    return -1;
+  }
 
-	if (!text_content)
-		text_content = "";
+  FILE *file = fopen(filename, "a");
+  if (!file) {
+    return -1;
+  }
 
-	fd = open(filename, O_RDWR | O_APPEND);
-	if (fd == -1)
-		return (-1);
+  if (!text_content) {
+    fclose(file);
+    return 1;
+  }
 
-	for (i = 0; text_content[i]; i++)
-		;
+  int result = fputs(text_content, file);
+  if (result < 0) {
+    return -1;
+  }
 
-	wcount = write(fd, text_content, i);
-	if (wcount == -1)
-		return (-1);
+  fclose(file);
 
-	close(fd);
-
-	return (1);
+  return 1;
 }
+
